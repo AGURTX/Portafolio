f1d4e427593c63e40a57f1092441a3c975b06f5e
AGURTX <137475039+AGURTX@users.noreply.github.com>
2025-11-28 14:29:14 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+   // Función para cambiar el color del título    
+        function cambiarColor(elemento){
+            elemento.style.color = "blue"; // Cambiar el color del texto a rojo
+    }
+
+    // Función para restaurar el color original del título  
+    function restaurarColor(elemento){
+            elemento.style.color = "black"; // Cambiar el color del texto a azul        
+    }
+
+    // Obtener el elemento del título por su ID
+    const bvb = document.getElementById("bvb");
+            
+    // Agregar eventos para cambiar el color cuando se pasa el mouse por encima y restaurar el color cuando se saca el mouse
+            
+    bvb.addEventListener("mouseover", function () { 
+        
+        cambiarColor(bvb);
+        
+    });
+            
+    bvb.addEventListener("mouseout", function () { 
+        
+        restaurarColor(bvb);
+        
+    });
+
+       // Función para cambiar el color del título    
+        function cambiarColor(elemento){
+            elemento.style.color = "blue"; // Cambiar el color del texto a rojo
+    }
+
+    // Función para restaurar el color original del título  
+    function restaurarColor(elemento){
+            elemento.style.color = "black"; // Cambiar el color del texto a azul        
+    }
+
+    // Obtener el elemento del título por su ID
+    const bvb1 = document.getElementById("bvb1");
+            
+    // Agregar eventos para cambiar el color cuando se pasa el mouse por encima y restaurar el color cuando se saca el mouse
+            
+    bvb1.addEventListener("mouseover", function () { 
+        
+        cambiarColor(bvb1);
+        
+    });
+            
+    bvb1.addEventListener("mouseout", function () { 
+        
+        restaurarColor(bvb1);
+        
+    });
+
+    // Función para cambiar el color del título    
+        function cambiarColor(elemento){
+            elemento.style.color = "blue"; // Cambiar el color del texto a rojo
+    }
+
+    // Función para restaurar el color original del título  
+    function restaurarColor(elemento){
+            elemento.style.color = "black"; // Cambiar el color del texto a azul        
+    }
+
+    // Obtener el elemento del título por su ID
+    const bvb2 = document.getElementById("bvb2");
+            
+    // Agregar eventos para cambiar el color cuando se pasa el mouse por encima y restaurar el color cuando se saca el mouse
+            
+    bvb2.addEventListener("mouseover", function () { 
+        
+        cambiarColor(bvb2);
+        
+    });
+            
+    bvb2.addEventListener("mouseout", function () { 
+        
+        restaurarColor(bvb2);
+        
+    });
+
+       // Función para cambiar el color del título    
+        function cambiarColor(elemento){
+            elemento.style.color = "blue"; // Cambiar el color del texto a rojo
+    }
+
+    // Función para restaurar el color original del título  
+    function restaurarColor(elemento){
+            elemento.style.color = "black"; // Cambiar el color del texto a azul        
+    }
+              
+                            
+                    
